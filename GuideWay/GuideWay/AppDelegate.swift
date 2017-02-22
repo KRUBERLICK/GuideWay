903163b97e93bc8e2d2d9a391a45b108c0630558
KRUBERLICK <kruberlick@gmail.com>
2017-02-22 13:41:33 +0200

    Initial commit

---
+++ GuideWay/GuideWay/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GuideWay
+//
+//  Created by Daniel Ilchishyn on 2/22/17.
+//  Copyright © 2017 Sasha&Daniel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ GuideWay/GuideWay/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GuideWay
+//
+//  Created by Daniel Ilchishyn on 2/22/17.
+//  Copyright © 2017 Sasha&Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
