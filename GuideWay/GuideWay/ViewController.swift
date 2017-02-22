65b1292ff4f650714eabebc766e080baf22ef7d8
KRUBERLICK <kruberlick@gmail.com>
2017-02-22 13:50:08 +0200

    Init cocoapods, add AsyncDisplayKit

--- GuideWay/GuideWay/ViewController.swift
+++ GuideWay/GuideWay/ViewController.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2017 Sasha&Daniel. All rights reserved.
 //
 
-import UIKit
+import AsyncDisplayKit
 
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
+class ViewController: ASViewController<ASDisplayNode> {
+    init() {
+        super.init(node: ASDisplayNode())
+        node.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
